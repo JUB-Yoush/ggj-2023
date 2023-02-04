f9ea2a4dcce1e78e0050cb0b962be49b4ec6f38d
JUB_Yoush <Yoush1337@gmail.com>
2023-02-03 21:04:45 -0500

    i forgor platformer

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -1,6 +1,16 @@
 extends KinematicBody2D
 
 var speed:= 10.0
+var velocity:= Vector2.ZERO
 var input_vector := Vector2.ZERO
 
 
+
+func get_input() -> void:
+    input_vector = Input.get_vector("left","right","up","down")
+
+func _physics_process(delta: float) -> void:
+    get_input()
+    velocity.x = input_vector.x
+
+    move_and_slide(velocity)
